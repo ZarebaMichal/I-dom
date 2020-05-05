a66ac29dbad3fba1b136153dbdd27ab8a2fd2276
Kacper <kackoh@st.amu.edu.pl>
2020-05-06 01:03:40 +0200

    Views changed. Added API. Need to test new API.

--- register/urls.py
+++ register/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_list, name='index')
+    path('register/', views.register_list),
+    path('register/<int:pk>', views.register_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
