a5ed51ac53e645822508c6964e556ac76710eb96
MrFoka27 <miczar1@st.amu.edu.pl>
2020-05-06 16:41:02 +0200

    Added token auth API, added new url in urls.py, changed telephone field from Char to PhoneNumberField() in models.py
    Signed-off-by: MrFoka27 <miczar1@st.amu.edu.pl>

--- register/models.py
+++ register/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 from .managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=25, unique=True)
     email = models.EmailField(unique=True)
-    telephone = models.CharField(max_length=9, unique=True)
+    telephone = PhoneNumberField()
     sms_notifications = models.BooleanField(default=True)
     app_notifications = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
--- register/urls.py
+++ register/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     path('register/', views.register_list),
-    path('register/<int:pk>', views.register_detail)
+    path('register/<int:pk>', views.register_detail),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
