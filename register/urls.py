cd51a03cfa3858d9148da4345577488b475c81da
Kacper <kackoh@st.amu.edu.pl>
2020-05-29 01:30:51 +0200

    In register/views.py: - now every request is in another method In register/urls.py: - every method from view.py has another url - now in /users/detail we can call user by username In register/test.py: - change url for every test to new urls - in everywhere where id was hardcoded change to dynamic getting id In sensor/views.py: - now every request is in another method In sensor/urls.py: - every method from view.py has another url In sensor/test.py: - change url for every test to new urls #TODO: something wrong with test to update sensor

--- register/urls.py
+++ register/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include, re_path
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
+
 
 urlpatterns = [
     # API paths
-    path('register/', views.register_list),
-    path('register/<int:pk>', views.register_detail),
+    path('users/list', views.users_list),
+    path('users/add', views.register_user),
+    path('users/detail/<str:username>', views.user_detail),
+    path('users/update/<int:pk>', views.update_user),
     path('users/delete/<int:pk>', views.delete_user),
     re_path(r'^api-logout/(?P<token>[a-zA-Z0-9]+)$', views.logout_user),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
