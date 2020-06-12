242445c3a90eae475af6a112b53a1a149e43bbfa
ZarebaMichal <zareba.michal@outlook.com>
2020-06-12 10:31:47 +0200

    Minor permissions fix
    Signed-off-by: ZarebaMichal <zareba.michal@outlook.com>

--- register/permissions.py
+++ register/permissions.py
@@ -8,13 +8,26 @@ class IsUpdateProfile(permissions.BasePermission):
     def has_permission(self, request, view):
         # can write custom code
         # print view.kwargs
-        try:
-            user_profile = CustomUser.objects.get(
-                pk=view.kwargs['pk'])
-        except ObjectDoesNotExist:
+        if view.kwargs['pk']:
+            try:
+                user_profile = CustomUser.objects.get(
+                    pk=view.kwargs['pk'])
+            except ObjectDoesNotExist:
+                return False
+
+            if request.user == user_profile:
+                return True
+
+            return False
+        else:
+            try:
+                user_profile = CustomUser.objects.get(
+                    pk=view.kwargs['username'])
+            except ObjectDoesNotExist:
+                return False
+
+            if request.user == user_profile:
+                return True
+
             return False
 
-        if request.user == user_profile:
-            return True
-
-        return False
