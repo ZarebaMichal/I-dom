ccdd514ff23f9cfd9493127ab23f3192e3b21076
ZarebaMichal <zareba.michal@outlook.com>
2020-11-30 19:29:21 +0100

    Fix
    Signed-off-by: ZarebaMichal <zareba.michal@outlook.com>

--- driver/views.py
+++ driver/views.py
@@ -1,3 +1,4 @@
+from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
